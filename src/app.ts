0a8f21667b8542dc52fdf7c24eed44ff41ac9b53
Oleksandr Verdiyev <sasha.verdiyev@gmail.com>
2022-07-22 21:19:53 +0200

    abstract component class

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,37 @@
+abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+  templateEl: HTMLTemplateElement;
+  hostEl: T;
+  element: U;
+
+  constructor(
+    templateId: string,
+    hostId: string,
+    insertAtStart: boolean,
+    newElementId?: string
+  ) {
+    this.templateEl = <HTMLTemplateElement>document.getElementById(templateId);
+    this.hostEl = <T>document.getElementById(hostId);
+
+    const importedNode = document.importNode(this.templateEl.content, true);
+    this.element = <U>importedNode.firstElementChild;
+    if (newElementId) {
+      this.element.id = newElementId;
+    }
+
+    this.attach(insertAtStart);
+  }
+
+  private attach(insertAtStart: boolean) {
+    this.hostEl.insertAdjacentElement(
+      insertAtStart ? 'afterbegin' : 'beforeend',
+      this.element
+    );
+  }
+
+  abstract configure(): void;
+  abstract renderContent(): void;
+}
+
 type Listener = (items: Project[]) => void;
 
 enum ProjectStatus {
